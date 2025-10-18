bf6da82362835ac2b0623dda365dc15d54928ff1
aelmansour <aelmansouri@ivytech.edu>
2025-10-17 23:45:23 -0400

    Add files via upload

---
+++ Module 8 Assignment - Functions in Python - Abdulaziz Elmansouri.py
@@ -0,0 +1,15 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> def greaterThan(x,y):
+...     if x > y:
+...         return True
+...     else:
+...         return False
+...     a = 2
+...     b = 3
+...     result = greaterThan(a,b)
+...     print("The statement " + str(a) + " is greater Than " + str(b) + str(result))
+...     a = 10
+...     b = 6
+...     result = greaterThan(a,b)
+...     print ("The statement " + str(a) + " is greater than " + str(b) + str(result))
